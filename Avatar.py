4e8b3ccd08bcdbe8c715667d63e66299ca9cf27d
mayank-genesis <mayank16meena@gmail.com>
2020-05-21 12:16:10 +0530

    ->Node.js trial successful

--- Avatar.py
+++ Avatar.py
@@ -18,9 +18,9 @@ versionDescription = A.get_text()
 version = str(versionDescription)
 version = version.lstrip()
 currentVersion = version[0:7]
-oldVersion = "12.16.2"
+oldVersion = "12.16.0"
 if(currentVersion != oldVersion):
-    updateSystem = input("We found new update for Node.js  "+oldVersion+" LTS. Do you want to Update Node.js to "+currentVersion+" version yes (yes/no): ")
+    updateSystem = input("We found new update for Node.js  v"+oldVersion+". Update Node.js to v"+currentVersion+"  yes (yes/no): ")
     if(updateSystem == "yes"):
         # print(updateSystem)
         webbrowser.open('https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi')
