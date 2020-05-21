db8b1ede13722492f965162e53a995aab5693874
mayank-genesis <mayank16meena@gmail.com>
2020-05-21 13:35:23 +0530

    NodeJS trial Successful

--- Avatar.py
+++ Avatar.py
@@ -17,14 +17,15 @@ versionDescription = A.get_text()
 version = str(versionDescription)
 version = version.lstrip()
 currentVersion = version[0:7]
-oldVersion = "12.16.3"
+oldVersion = output
 
 if(currentVersion != oldVersion):
-    updateSystem = input("We found new update for NodeJS "+oldVersion+". Download v"+currentVersion+" yes (yes/no): ")
+    updateSystem = input("We found new updates for NodeJS "+oldVersion+"Download v"+currentVersion+" (yes/no): ")
     if(updateSystem == "yes"):
         webbrowser.open('https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi')
         oldVersion = currentVersion
-else : exitKey = input("No Updates available for NodeJS "+output+'\n'+"Hit Enter to exit Avatar: ")
+else : exitKey = input("No Updates available for NodeJS "+oldVersion+'\n'+"Hit Enter to exit Avatar: ")
+
 
 print("Closing Avatar")        
 
