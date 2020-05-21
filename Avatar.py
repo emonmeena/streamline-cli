c28a7e56d215e04f9df79cc467c3ee7ac6dfc34b
mayank-genesis <mayank16meena@gmail.com>
2020-05-21 17:34:05 +0530

    ->availability bug-fixed

--- Avatar.py
+++ Avatar.py
@@ -2,16 +2,21 @@ import requests
 from bs4 import BeautifulSoup 
 import webbrowser
 import os
-import LogIn
+# import LogIn
 
-LogIn
-
+print(" \t Welcome to Avatar ")
 url = 'https://nodejs.org/en/'
+
 stream = os.popen('Node -v')
 output = stream.read()
+output = output.strip()
+# output = str(output)
 # os.system('Node --version')
-print("Searching for Updates...")
-r = requests.get(url)
+print(" Searching for any Tech Updates...")
+try:
+    r = requests.get(url)
+except:
+       romErr = input("\n Internet Connection is required \n"+" Hit Enter to exit Avatar")
 htmlContent = r.content
 
 soup = BeautifulSoup(htmlContent, 'html.parser')
@@ -19,17 +24,31 @@ A = soup.find("a", class_="home-downloadbutton")
 versionDescription = A.get_text()
 version = str(versionDescription)
 version = version.lstrip()
-currentVersion = version[0:7]
+currentVersion = "v"+version[0:7]
 oldVersion = output
 
-if(currentVersion != oldVersion):
-    updateSystem = input("We found new updates for NodeJS "+oldVersion+"Download v"+currentVersion+" (yes/no): ")
+if(currentVersion != oldVersion and len(currentVersion) == len(oldVersion)):
+    updateSystem = input(" We found new updates for NodeJS "+oldVersion+" Download "+currentVersion+" (yes/no): ")
     if(updateSystem == "yes"):
-        webbrowser.open('https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi')
-        oldVersion = currentVersion
-else : exitKey = input("No Updates available for NodeJS "+oldVersion+'\n'+"Hit Enter to exit Avatar: ")
+     try:
+        downloadFile = webbrowser.open('https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi')
+     except:
+        romErr2 = input(" Oops! Internet Connection lost. Reconnect, than try again later \n")
+    oldVersion = currentVersion
 
-print("Closing Avatar")        
+elif(currentVersion == oldVersion):
+     exitKey = input(" NodeJS "+oldVersion+" is up to date"+'\n'+" Hit Enter to exit Avatar: ")
+
+else: 
+    downloadSystem = input(" \n Seems like You don't have NodeJS installed on your machine. Download NodeJS "+currentVersion+" (yes/no): ")
+    if(downloadSystem == "yes"):
+     try:
+        downloadFile = webbrowser.open('https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi')
+     except:
+        romErr2 = input(" Oops! Internet Connection lost. Reconnect, then try. \n")
+    oldVersion = currentVersion
+
+print(" Closing Avatar")        
 
 
 
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 * I saw the need for a software which can search for the updates for dev-techs automatically and can download them.
 ## References
 * Shell commands in Python Interpreter = https://janakiev.com/blog/python-shell-commands/
+* Checking Internet Connection = https://www.programiz.com/python-programming/exception-handling
