306255185665a6440e6fc23a2bc7beaa5c06ac6d
mayank-genesis <mayank16meena@gmail.com>
2020-05-23 03:07:20 +0530

    ->Comments Added

--- Avatar.py
+++ Avatar.py
@@ -7,6 +7,7 @@ import os
 print(" \t Welcome to Avatar ")
 urlSite = 'https://nodejs.org/en/'
 techStatus = " Searching for any Tech Updates..."
+# System Config
 stream = os.popen('Node -v')
 output = stream.read()
 output = output.strip()
@@ -19,6 +20,7 @@ except:
        romErr = input("\n Internet Connection is required \n"+" Hit Enter to exit Avatar")
 htmlContent = r.content
 
+# Web-Scraping
 soup = BeautifulSoup(htmlContent, 'html.parser')
 A = soup.find("a", class_="home-downloadbutton")
 versionDescription = A.get_text()
@@ -26,7 +28,7 @@ version = str(versionDescription)
 version = version.lstrip()
 currentVersion = "v"+version[0:7]
 oldVersion = output
-urlDownload = 'https://nodejs.org/dist/'+currentVersion+'/node-v12.16.3-x64.msi'
+urlDownload = 'https://nodejs.org/dist/'+currentVersion+'/node-'+currentVersion+'-x64.msi'
 
 # Defining Input Strings
 errorConnection = " Oops! Internet Connection lost. Reconnect, then try. \n"
@@ -34,6 +36,7 @@ noUpdaterequire = " NodeJS "+oldVersion+" is up to date"+'\n'+" Hit Enter to exi
 update = " We found new updates for NodeJS "+oldVersion+" Download "+currentVersion+" (yes/no): "
 noMatchtoDownload = " \n Seems like NodeJS is not installed in this machine. Install or Download the latest release, NodeJS "+currentVersion+" (yes/no): "
 
+# Checking for Updates
 if(currentVersion != oldVersion and len(currentVersion) == len(oldVersion)):
     updateSystem = input(update)
     if(updateSystem == "yes"):
@@ -54,6 +57,7 @@ else:
      except:
         romErr2 = input(errorConnection)
     oldVersion = currentVersion
+#Closing Avatar 
 print(" Closing Avatar")        
 
 
